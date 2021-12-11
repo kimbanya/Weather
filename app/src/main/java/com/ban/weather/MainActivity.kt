c0b638efb9e7ba02b35d5ecfea2d7c0b956f8469
Banya Kim <kimbanya@gmail.com>
2021-12-11 20:23:00 +0900

    Revise view using view binding

--- app/src/main/java/com/ban/weather/MainActivity.kt
+++ app/src/main/java/com/ban/weather/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.ban.weather
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
+import androidx.annotation.RequiresApi
+import com.ban.weather.databinding.ActivityMainBinding
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -14,6 +17,9 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Path
+import java.text.DateFormat
+import java.time.LocalDate
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,11 +29,14 @@ class MainActivity : AppCompatActivity() {
         ApiInterface.create()
     }
 
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        // view binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         getCityWeather(44418)
 
@@ -48,9 +57,32 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun updateView(result: WeatherResponseModel?) {
-        val title: TextView = findViewById(R.id.tv_location_title)
+        val localDate: LocalDate = LocalDate.now()
+
+        val title = binding.tvLocationTitle
+        val theTemp = binding.tvTheTemp
+        val weatherState = binding.tvWeatherState
+        val maxTemp = binding.tvMaxTemp
+        val minTemp = binding.tvMinTemp
+
+        val tomorrowDay = binding.tvTomorrowDay
+        val tomorrowWeatherIcon = binding.ivTomorrowWeatherIcon
+        val tomorrowWeatherStatus = binding.tvTomorrowWeatherStatus
+        val tomorrowHighLow = binding.tvTomorrowHighLow
+
         title.text = result!!.title
+        theTemp.text = result!!.consolidatedWeather[0].theTemp.toString()
+        weatherState.text = result!!.consolidatedWeather[0].weatherStateName
+        maxTemp.text = result!!.consolidatedWeather[0].maxTemp.toString()
+        minTemp.text = result!!.consolidatedWeather[0].minTemp.toString()
+
+        tomorrowDay.text = "${localDate.monthValue}/${localDate.dayOfMonth}"
+//        tomorrowWeatherIcon. = result!!.consolidatedWeather[1].weatherStateAbbr
+        tomorrowWeatherStatus.text = result!!.consolidatedWeather[1].weatherStateName
+        tomorrowHighLow.text = result!!.consolidatedWeather[1].maxTemp.toString() + "/" + result!!.consolidatedWeather[1].minTemp.toString()
+
     }
 
 }
