07b9291cb0d067c0da46048b571368f14ee5eeda
Banya Kim <kimbanya@gmail.com>
2021-12-11 18:57:19 +0900

    Add retrofit to get data from the internet

--- app/src/main/java/com/ban/weather/MainActivity.kt
+++ app/src/main/java/com/ban/weather/MainActivity.kt
@@ -2,13 +2,78 @@ package com.ban.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    private val TAG = javaClass.simpleName
+
+    private val retrofit: ApiInterface by lazy {
+        ApiInterface.create()
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
 
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        getCityWeather(44418)
+
+    }
+
+    fun getCityWeather(woeid: Long) {
+        GlobalScope.launch {
+            withContext(Dispatchers.IO) {
+                val response = retrofit.getWeatherById(woeid) // woeid.toString()
+                if (response.isSuccessful) {
+                    Log.d(TAG, "[getCityWeather]")
+                    updateView(response.body())
+                } else {
+                    Log.d(TAG, "[Fail to getCityWeather]")
+
+                }
+            }
+        }
+    }
+
+    private fun updateView(result: WeatherResponseModel?) {
+        val title: TextView = findViewById(R.id.tv_location_title)
+        title.text = result!!.title
+    }
 
 }
+
+interface ApiInterface {
+
+
+    companion object {
+
+        var BASE_URL = "https://www.metaweather.com/api/"
+
+        fun create(): ApiInterface {
+
+            val retrofit = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+            return retrofit.create(ApiInterface::class.java)
+        }
+    }
+
+    @GET("location/{woeid}")
+    suspend fun getWeatherById(@Path("woeid") woeid: Long): Response<WeatherResponseModel>
+
+}
+
---
+++ app/src/main/java/com/ban/weather/WeatherResponseModel.kt
@@ -0,0 +1,98 @@
+package com.ban.weather
+
+import com.google.gson.annotations.SerializedName
+
+
+data class WeatherResponseModel(
+    @SerializedName("consolidated_weather")
+    val consolidatedWeather: List<ConsolidatedWeatherModel>,
+
+    val time: String,
+
+    @SerializedName("sun_rise")
+    val sunRise: String,
+
+    @SerializedName("sun_set")
+    val sunSet: String,
+
+    @SerializedName("timezone_name")
+    val timezoneName: String,
+
+    val parent: Parent,
+    val sources: List<Source>,
+    val title: String,
+
+    @SerializedName("location_type")
+    val locationType: String,
+
+    val woeid: Long,
+
+    @SerializedName("latt_long")
+    val lattLong: String,
+
+    val timezone: String
+)
+
+data class ConsolidatedWeatherModel(
+
+    val id: Long,
+
+    @SerializedName("weather_state_name")
+    val weatherStateName: String,
+
+    @SerializedName("weather_state_abbr")
+    val weatherStateAbbr: String,
+
+    @SerializedName("wind_direction_compass")
+    val windDirectionCompass: String,
+
+    val created: String,
+
+    @SerializedName("applicable_date")
+    val applicableDate: String,
+
+    @SerializedName("min_temp")
+    val minTemp: Double,
+
+    @SerializedName("max_temp")
+    val maxTemp: Double,
+
+    @SerializedName("the_temp")
+    val theTemp: Double,
+
+    @SerializedName("wind_speed")
+    val windSpeed: Double,
+
+    @SerializedName("wind_direction")
+    val windDirection: Double,
+
+    @SerializedName("air_pressure")
+    val airPressure: Double,
+
+    val humidity: Long,
+    val visibility: Double,
+    val predictability: Long
+
+
+)
+
+data class Parent(
+    val title: String,
+
+    @SerializedName("location_type")
+    val locationType: String,
+
+    val woeid: Long,
+
+    @SerializedName("latt_long")
+    val lattLong: String
+)
+
+data class Source(
+    val title: String,
+    val slug: String,
+    val url: String,
+
+    @SerializedName("crawl_rate")
+    val crawlRate: Long
+)
